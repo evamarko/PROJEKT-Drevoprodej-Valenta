a610fafc54e393727b01f5fe8e7c31552a931079
Eva Marko <valentova.eva92@gmail.com>
2025-07-19 22:48:09 -0600

    Přidáno vyskakovací okno

--- js/app.js
+++ js/app.js
@@ -1,29 +1,12 @@
-function coll() {
-  let button = document.getElementById("button");
-  let content = document.getElementById("moreServices");
-
-  if (content.style.display === "block") {
-    content.style.display = "none";
-  } else {
-    content.style.display = "block";
-  }
-  button.classList.toggle("active");
+function openPopup() {
+  document.getElementById("myPopup").style.display = "block";
 }
 
-$(window).ready(function () {
-  var windowWidth = $(window).width();
-  if (windowWidth > 540) {
-    $(".menu__item--hover").mouseenter(function () {
-      $(".menu__dropdown").css("display", "block");
-    });
-    $(".menu__dropdown").mouseenter(function () {
-      $(".menu__dropdown").css("display", "block");
-    });
-    $(".menu__item--hover").mouseleave(function () {
-      $(".menu__dropdown").css("display", "none");
-    });
-    $(".menu__dropdown").mouseleave(function () {
-      $(".menu__dropdown").css("display", "none");
-    });
-  }
-});
+function closePopup() {
+  document.getElementById("myPopup").style.display = "none";
+}
+
+// Zobrazení popupu při načtení stránky
+window.onload = function () {
+  openPopup();
+};
